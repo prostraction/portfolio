d585a6f08a0448f6682dfd76393af6c31a1bd24a
prostraction <47314760+prostraction@users.noreply.github.com>
2023-06-05 15:52:22 +0300

    JS skills generation

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const skills = {
+    data: [{
+        item: "C++",
+        level: 100,
+        iconPath: "img/skills/c++.svg",
+    },
+    {
+        item: "Go",
+        level: 80,
+        iconPath: "img/skills/golang.svg",
+    },
+    {
+        item: "JS",
+        level: 50,
+        iconPath: "img/skills/javascript.svg",
+    },
+    {
+        item: "HTML",
+        level: 100,
+        iconPath: "img/skills/html.svg",
+    },
+    {
+        item: "CSS",
+        level: 40,
+        iconPath: "img/skills/css.svg",
+    }],
+    generateList: function(parentElement) {
+        this.data.forEach(function(element) {
+            const dt = document.createElement('dt');
+            dt.classList.add('skill-item');
+            dt.textContent = element.item;
+            dt.style.backgroundImage = `url(${element.iconPath})`;
+
+            const dd = document.createElement('dd');
+            dd.classList.add('skill-level');
+            
+            const div = document.createElement('div');
+            div.style.width = `${element.level}%`;
+            div.textContent = `${element.level}%`;
+
+            dd.appendChild(div);
+            parentElement.appendChild(dt);
+            parentElement.appendChild(dd);
+        });
+    }
+};
+
+skills.generateList(document.querySelector('dl.skill-list'));
